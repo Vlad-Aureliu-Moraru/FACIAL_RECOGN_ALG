5f5772ee7ad6f7ceda07afdf841d4617b19cac5e
thorn <moraru.vlad34@gmail.com>
2025-12-16 00:44:45 +0200

    M3 UI Improved

--- M3_GUI.py
+++ M3_GUI.py
@@ -3,27 +3,143 @@ from tkinter import ttk
 import M2_helper as m2h
 from functools import partial
 
+
+def select_photos():
+    adapt_input_panel()
+    num_test_images = 160
+    if train_var.get() == '70%':
+        num_test_images = 120
+    elif train_var.get() == '80%':
+        num_test_images = 80
+
+    for widget in photo_panel.winfo_children():
+        widget.destroy()
+
+    for i in range(num_test_images):
+        btn = Button(photo_panel, text=f"Photo {i+1}",
+                     width=5, command=partial(run_single_test, i))
+        row = i //12
+        col = i % 12
+        btn.grid(row=row, column=col, padx=2, pady=3)
+
+def run_single_test(index):
+    print(f"Running single test for Photo {index+1}")
+    if algo_var.get() == 'Eigenfaces clasic':
+        m2h.run_single_eigenface(index,graph_panel,train_var.get(),k_var.get())
+    elif algo_var.get()=='Eigenfaces cu repr. de clasa':
+        m2h.run_single_eigenface_classrep(index,graph_panel,train_var.get(),k_var.get())
+    elif algo_var.get()=='Lanczos':
+        m2h.run_single_lanczos(index,graph_panel,train_var.get(),k_var.get())
+
+def adapt_input_panel():
+    for widget in input_panel.winfo_children():
+        widget.destroy()
+
+    if algo_var.get() == "NN":
+            print("NN PICKED")
+            
+            ttk.Label(input_panel, text="NN Norma:").pack(padx=5, pady=5, side=TOP)
+            
+            norm_combobox = ttk.Combobox(
+                input_panel, 
+                textvariable=nn_norm_var, 
+                values=list(range(1, 5)), 
+                state="readonly",
+                width=5
+            )
+            norm_combobox.pack(padx=5, pady=5, side=TOP)
+            
+            norm_combobox.set(nn_norm_var)
+    elif algo_var.get() == "KNN":
+        print("KNN PICKED")
+        
+        ttk.Label(input_panel, text="NN Norm:").pack(padx=5, pady=5, side=TOP)
+        
+        norm_combobox = ttk.Combobox(
+            input_panel, 
+            textvariable=nn_norm_var, 
+            values=list(range(1, 5)), 
+            state="readonly",
+            width=5
+        )
+        norm_combobox.pack(padx=5, pady=5, side=TOP)
+
+        ttk.Label(input_panel, text="K valori:").pack(padx=5, pady=5, side=TOP) # <<< Added Label
+        
+        k_combobox = ttk.Combobox(
+            input_panel, 
+            textvariable=knn_k_var, 
+            values=[3, 5, 7], 
+            state="readonly",
+            width=5
+        )
+        k_combobox.pack(padx=5, pady=5, side=TOP)
+        
+        norm_combobox.set(nn_norm_var) 
+        k_combobox.set(knn_k_var)
+    elif algo_var.get() in['Eigenfaces clasic','Eigenfaces cu repr. de clasa','Lanczos'] :
+        print(f"{k_var.get()} TO SS")
+
+
+        k_label = ttk.Label(input_panel, text="Nr de eigenfaces (K):", width=23, wraplength=300,font=("Arial",11))
+        k_label.pack(padx=5, pady=5, side=TOP)
+
+        k_slider = Scale(input_panel, from_=1, to=100, orient=HORIZONTAL, variable=k_var)
+        k_slider.pack(padx=5, pady=5, side=TOP)
+        
+        
+        ttk.Label(input_panel, text="NN Norm:").pack(padx=5, pady=5, side=TOP)
+        
+        norm_combobox = ttk.Combobox(
+            input_panel, 
+            textvariable=nn_norm_var, 
+            values=list(range(1, 5)), 
+            state="readonly",
+            width=5
+        )
+        norm_combobox.pack(padx=5, pady=5, side=TOP)
+        
+        norm_combobox.set(nn_norm_var)
+
 window = Tk()
-window.geometry("840x800")
+window.geometry("900x800")
 window.title("ML Algoritmi")
+
+k_var = IntVar(value=20)  
+nn_norm_var = IntVar(value=1)
+knn_k_var = IntVar(value=3)
+algo_var = StringVar(value="NN") 
 
 # ---------- Control Panel ----------
 control_panel = ttk.Frame(window, relief=RAISED, borderwidth=1)
-control_panel.place(x=20, y=20, width=800, height=100)
+control_panel.place(x=20, y=20, width=860, height=100)
 
 # ---------- Single Photo Panel ----------
-single_photo_panel= ttk.Frame(window, relief=RAISED, borderwidth=1)
-single_photo_panel.place(x=20, y=140, width=800, height=150)
 
+canvas = Canvas(window)
+scrollbar = Scrollbar(window, orient=VERTICAL, command=canvas.yview)
+photo_panel = Frame(canvas, relief=RAISED, borderwidth=1)
+
+photo_panel.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+
+canvas.create_window((0, 0), window=photo_panel, anchor="nw")
+canvas.configure(yscrollcommand=scrollbar.set)
+
+
+canvas.place(x=20, y=140, width=860, height=150)
+scrollbar.place(x=870, y=140, height=150)
 
 # ---------- input_panel ----------
 input_panel= ttk.Frame(window, relief=RAISED, borderwidth=1)
-input_panel.place(x=20, y=310, width=300, height=400)
+input_panel.place(x=20, y=310, width=150, height=400)
 
 
 # ---------- statistic_panel ----------
 statistic_panel= ttk.Frame(window, relief=RAISED, borderwidth=1)
-statistic_panel.place(x=340, y=310, width=480, height=400)
+statistic_panel.place(x=180, y=310, width=680, height=400)
 
 # ---------- Combobox Algoritmi----------
 
@@ -37,7 +153,7 @@ algo_combo['values'] = ('Eigenfaces clasic', 'Eigenfaces cu repr. de clasa', 'La
 algo_combo.current(0)  
 algo_combo.place(x=300, y=10)
 
-apply_alg_btn = ttk.Button(control_panel,text="Aplica",width=30)
+apply_alg_btn = ttk.Button(control_panel,text="Aplica",width=30,command=select_photos)
 apply_alg_btn.place(x=530,y=10)
 
 # ---------- Split The DB----------
@@ -62,5 +178,6 @@ search_text_box.place(x=600, y=50)
 
 search_btn = ttk.Button(control_panel,text=" ",width=5)
 search_btn.place(x=700,y=50)
+
 
 window.mainloop()
